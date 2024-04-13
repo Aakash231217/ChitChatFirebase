8933f077fa060bae32d2685658922219a1f0a8e1
Aakash <rktaakash@gmail.com>
2024-04-13 22:49:02 +0530

    minor changes

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -1,16 +1,44 @@
-import React from 'react';
+import React,{useEffect} from 'react';
 import { useLocation } from 'react-router-dom';
+import socketIo from "socket.io-client";
+import './Chat.css';
 
+const ENDPOINT = "http://localhost:4500/";
+
+const useQuery = ()=>{
+    return new URLSearchParams(useLocation().search);
+}
 const Chat = () => {
-  const location = useLocation();
-
-  // A function to parse query parameters
-  const searchParams = new URLSearchParams(location.search);
-  const userName = searchParams.get('Uname');
-
+    const query = useQuery();
+    const name = query.get('Uname');
+  const socket = socketIo(ENDPOINT,{transports:['websocket']})
+  useEffect(()=>{const socket = socketIo(ENDPOINT,{transports:['websocket']})
+    socket.on('connect',()=>{
+        alert("Connected");
+    })
+    socket.emit('joined',{ name })
+    socket.on('welcome',(data)=>{
+        console.log(data.name,data.message)
+    })
+    socket.on('userJoined',(data)=>{
+        console.log(data.name, data.message)
+    })
+    return ()=>{
+     socket.emit('disconnect')
+     socket.off();
+    }
+  },[socket,name])
   return (
-    <div>
-      <h1>{userName}</h1>
+    <div className="chatPage">
+        <div className="chatContainer">
+            <div className="header">
+            </div>
+            <div className="chatBox"></div>
+            <div className="inputBox">
+                <input type="text" id="chatInput"/>
+                <button className="sendBtn">Send</button>
+            </div>
+        </div>
     </div>
   );
 }
